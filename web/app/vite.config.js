5e0dcca538eccf745379f276ee765a60a24869db
InternalBenchmarkDebricked <101089214+InternalBenchmarkDebricked@users.noreply.github.com>
2025-01-13 14:52:24 +0100

    Update vite.config.js

--- web/app/vite.config.js
+++ web/app/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import htmlPurge from './src/vite-plugin-purgecss.ts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    htmlPurge({ ...options })
+  ]
 })
