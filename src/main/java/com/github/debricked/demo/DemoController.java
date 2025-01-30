426b246007618e85985dfb7d3c44f83a4976db14
InternalBenchmarkDebricked <101089214+InternalBenchmarkDebricked@users.noreply.github.com>
2025-01-30 14:38:09 +0100

    Update DemoController.java

--- src/main/java/com/github/debricked/demo/DemoController.java
+++ src/main/java/com/github/debricked/demo/DemoController.java
@@ -2,6 +2,10 @@ package com.github.debricked.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 @RestController
 public class DemoController {
@@ -11,4 +15,17 @@ public class DemoController {
 		return "Greetings from Spring Boot!";
 	}
 
+	public void printDebrickedTitles() {
+		Document doc = Jsoup.connect("https://debricked.com/").get();
+	        log(doc.title());
+	
+	        Elements newsHeadlines = doc.select("#mp-itn b a");
+	        for (Element headline : newsHeadlines) {
+	            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+	        }
+	}
+
+	private static void log(String msg, String... vals) {
+		System.out.println(String.format(msg, (Object[]) vals));
+    	}
 }
