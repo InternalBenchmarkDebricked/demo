ac02fd7553686f9db6d6c40a95e53f4b53e18314
InternalBenchmarkDebricked <101089214+InternalBenchmarkDebricked@users.noreply.github.com>
2025-01-30 16:03:08 +0100

    Update DemoController.java

--- src/main/java/com/github/debricked/demo/DemoController.java
+++ src/main/java/com/github/debricked/demo/DemoController.java
@@ -3,11 +3,22 @@ package com.github.debricked.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 @RestController
 public class DemoController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		Document doc = Jsoup.connect("https://debricked.com/").get();
+	
+	        Elements loginButton = doc.select("div[class^=journey-button] > *:contains(Log in)").first();
+
+		return "Please visit %s to log into Debricked!".formatted(loginButton.attr("href"); 
 	}
 }
