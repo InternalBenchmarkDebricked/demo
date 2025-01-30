ddbda4514f045713b714b074844510fd9f109f86
InternalBenchmarkDebricked <101089214+InternalBenchmarkDebricked@users.noreply.github.com>
2025-01-30 14:58:03 +0100

    Update DemoController.java

--- src/main/java/com/github/debricked/demo/DemoController.java
+++ src/main/java/com/github/debricked/demo/DemoController.java
@@ -3,13 +3,6 @@ package com.github.debricked.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
 @RestController
 public class DemoController {
 
@@ -17,18 +10,4 @@ public class DemoController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
-
-	public void printDebrickedTitles() throws IOException {
-		Document doc = Jsoup.connect("https://debricked.com/").get();
-	        log(doc.title());
-	
-	        Elements newsHeadlines = doc.select("#mp-itn b a");
-	        for (Element headline : newsHeadlines) {
-	            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
-	        }
-	}
-
-	private static void log(String msg, String... vals) {
-		System.out.println(String.format(msg, (Object[]) vals));
-    	}
 }
